409e0c81cb96d36ccb041c5ba86d1988f62db27a
David Hara <hakuhen@gmail.com>
2012-12-04 20:13:34 -0600

    Added a new activity and created the first pass at our main activity layout.

---
+++ Vinotory/src/com/harataira/vinotory/activity/AddWineBottleActivity.java
@@ -0,0 +1,14 @@
+package com.harataira.vinotory.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddWineBottleActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
--- Vinotory/src/com/harataira/vinotory/activity/MainActivity.java
+++ Vinotory/src/com/harataira/vinotory/activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.harataira.vinotory.activity;
 
-import com.harataira.vinotory.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+
+import com.harataira.vinotory.R;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +21,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void startAddBottleActivity(View view) {
+	    Intent intent = new Intent(this, AddWineBottleActivity.class);
+	    startActivity(intent);
 	}
 
 }
